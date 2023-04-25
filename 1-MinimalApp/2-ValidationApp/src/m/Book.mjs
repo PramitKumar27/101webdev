2337aceddb99106d10ac913f288da0aba70552f4
PramitKumar27 <pramitkumarbhaduri@gmail.com>
2023-04-25 16:19:28 +0200

    update3

--- 1-MinimalApp/2-ValidationApp/src/m/Book.mjs
+++ 1-MinimalApp/2-ValidationApp/src/m/Book.mjs
@@ -1,14 +1,14 @@
 /**
- * @fileOverview  The model class Book with attribute definitions and storage management methods
+ * @fileOverview  The model class movie with attribute definitions and storage management methods
  * @author Gerd Wagner
  * @copyright Copyright 2013-2014 Gerd Wagner, Chair of Internet Technology, Brandenburg University of Technology, Germany. 
  * @license This code is licensed under The Code Project Open License (CPOL), implying that the code is provided "as-is", 
  * can be modified to create derivative works, can be redistributed, and can be used in commercial applications.
  */
 /**
- * Constructor function for the class Book 
+ * Constructor function for the class movie 
  * @constructor
- * @param {{isbn: string, title: string, year: number, edition: number?}} slots
+ * @param {{movieId: Number, title: string, releaseDate: Year}} slots
  */
 import { isNonEmptyString, nextYear, isIntegerOrIntegerString, cloneObject }
   from "../../lib/util.mjs";
@@ -16,64 +16,64 @@ import { NoConstraintViolation, MandatoryValueConstraintViolation, RangeConstrai
   IntervalConstraintViolation, PatternConstraintViolation, UniquenessConstraintViolation }
   from "../../lib/errorTypes.mjs";
 
-function Book( slots) {
+function Movie( slots) {
   // assign default values
-  this.isbn = "";   // string
+  this.movieId = "";   // string
   this.title = "";  // string
-  this.year = 0;    // number (int)
+  this.releaseDate = 0;    // number (int)
   // this.edition   number (int) optional
   // set properties only if constructor is invoked with an argument
   if (arguments.length > 0) {
-    this.setIsbn( slots.isbn); 
+    this.setMovieId( slots.movieId); 
     this.setTitle( slots.title); 
-    this.setYear( slots.year);
-    if (slots.edition) this.setEdition( slots.edition);  // optional
+    this.setReleaseDate( slots.releaseDate);
+  // optional
   }
 }
 /*********************************************************
 ***  Class-level ("static") properties  ******************
 **********************************************************/
 // initially an empty collection (in the form of a map)
-Book.instances = {};
+Movie.instances = {};
 
 /*********************************************************
 ***  Checks and Setters  *********************************
 **********************************************************/
-Book.checkIsbn = function (id) {
+Movie.checkMovieId = function (id) {
   if (!id) return new NoConstraintViolation();
   else if (typeof(id) !== "string" || id.trim() === "") {
-    return new RangeConstraintViolation("The ISBN must be a non-empty string!");
+    return new RangeConstraintViolation("The Movie ID must be a non-empty string!");
   } else if (!(/\b\d{9}(\d|X)\b/.test( id))) {
     return new PatternConstraintViolation(
-        'The ISBN must be a 10-digit string or a 9-digit string followed by "X"!');
+        'The Movie ID must be a 10-digit string or a 9-digit string followed by "X"!');
   } else {
     return new NoConstraintViolation();
   }
 };
-Book.checkIsbnAsId = function (id) {
-  var validationResult = Book.checkIsbn( id);
+Movie.checkMovieIdAsId = function (id) {
+  var validationResult = Movie.checkMovieId( id);
   if ((validationResult instanceof NoConstraintViolation)) {
     if (!id) {
       validationResult = new MandatoryValueConstraintViolation(
-          "A value for the ISBN must be provided!");
-    } else if (Book.instances[id]) {  
+          "A value for the Movie ID must be provided!");
+    } else if (Movie.instances[id]) {  
       validationResult = new UniquenessConstraintViolation(
-          "There is already a book record with this ISBN!");
+          "There is already a movie record with this ID!");
     } else {
       validationResult = new NoConstraintViolation();
     } 
   }
   return validationResult;
 };
-Book.prototype.setIsbn = function (id) {
-  const validationResult = Book.checkIsbnAsId( id);
+Movie.prototype.setMovieId = function (id) {
+  const validationResult = Movie.checkMovieIdAsId( id);
   if (validationResult instanceof NoConstraintViolation) {
-    this.isbn = id;
+    this.movieId = id;
   } else {
     throw validationResult;
   }
 };
-Book.checkTitle = function (t) {
+Movie.checkTitle = function (t) {
   if (!t) {
     return new MandatoryValueConstraintViolation("A title must be provided!");
   } else if (!isNonEmptyString(t)) {
@@ -82,16 +82,16 @@ Book.checkTitle = function (t) {
     return new NoConstraintViolation();
   }
 };
-Book.prototype.setTitle = function (t) {
-  var validationResult = Book.checkTitle( t);
+Movie.prototype.setTitle = function (t) {
+  var validationResult = Movie.checkTitle( t);
   if (validationResult instanceof NoConstraintViolation) {
     this.title = t;
   } else {
     throw validationResult;
   }
 };
-Book.checkYear = function (y) {
-  const YEAR_FIRST_BOOK = 1459;
+Movie.checkReleaseDate = function (y) {
+  const YEAR_FIRST_MOVIE = 1895;
   if (!y) {
     return new MandatoryValueConstraintViolation(
 	    "A publication year must be provided!");
@@ -99,170 +99,170 @@ Book.checkYear = function (y) {
     return new RangeConstraintViolation("The value of year must be an integer!");
   } else {
     if (typeof y === "string") y = parseInt(y);
-    if (y < YEAR_FIRST_BOOK || y > nextYear()) {
+    if (y < YEAR_FIRST_MOVIE || y > nextYear()) {
       return new IntervalConstraintViolation(
-          `The value of year must be between ${YEAR_FIRST_BOOK} and next year!`);
+          `The value of year must be between ${YEAR_FIRST_MOVIE} and next year!`);
     } else {
       return new NoConstraintViolation();
     }
   }
 };
-Book.prototype.setYear = function (y) {
-  const validationResult = Book.checkYear( y);
+Movie.prototype.setReleaseDate = function (y) {
+  const validationResult = Movie.checkReleaseDate( y);
   if (validationResult instanceof NoConstraintViolation) {
-    this.year = parseInt( y);
+    this.releaseDate = parseInt( y);
   } else {
     throw validationResult;
   }
 };
-Book.checkEdition = function (e) {
-  // the "edition" attribute is optional
-  if (!e || e === "") return new NoConstraintViolation();
-  else {
-    if (!isIntegerOrIntegerString(e) || parseInt(e) < 1) {
-      return new RangeConstraintViolation(
-	      "The value of edition must be a positive integer!");
-    } else {
-      return new NoConstraintViolation();
-    }
-  }
-};
-Book.prototype.setEdition = function (e) {
-  var validationResult = Book.checkEdition( e);
-  if (validationResult instanceof NoConstraintViolation) {
-    if (!e || e === "") delete this.edition;  // unset optional property
-    else this.edition = parseInt( e);
-  } else {
-    throw validationResult;
-  }
-};
+// movie.checkEdition = function (e) {
+//   // the "edition" attribute is optional
+//   if (!e || e === "") return new NoConstraintViolation();
+//   else {
+//     if (!isIntegerOrIntegerString(e) || parseInt(e) < 1) {
+//       return new RangeConstraintViolation(
+// 	      "The value of edition must be a positive integer!");
+//     } else {
+//       return new NoConstraintViolation();
+//     }
+//   }
+// };
+// movie.prototype.setEdition = function (e) {
+//   var validationResult = movie.checkEdition( e);
+//   if (validationResult instanceof NoConstraintViolation) {
+//     if (!e || e === "") delete this.edition;  // unset optional property
+//     else this.edition = parseInt( e);
+//   } else {
+//     throw validationResult;
+//   }
+// };
 /*********************************************************
 ***  Other Instance-Level Methods  ***********************
 **********************************************************/
 /**
- *  Serialize book object 
+ *  Serialize movie object 
  */
-Book.prototype.toString = function () {
-  var bookStr = `Book{ ISBN: ${this.isbn}, title: ${this.title}, year: ${this.year}`;
-  if (this.edition) bookStr += `, edition: ${this.edition}`;
-  return bookStr;
+Movie.prototype.toString = function () {
+  var movieStr = `Movie{ movieId: ${this.movieId}, title: ${this.title}, releaseDate: ${this.releaseDate}`;
+  
+  return movieStr;
 };
 /*********************************************************
 ***  Class-level ("static") storage management methods ***
 **********************************************************/
 /**
- *  Create a new book row
+ *  Create a new movie row
  */
-Book.add = function (slots) {
-  var book = null;
+Movie.add = function (slots) {
+  var movie = null;
   try {
-    book = new Book( slots);
+    movie = new Movie( slots);
   } catch (e) {
     console.log( `${e.constructor.name}: ${e.message}`);
-    book = null;
+    movie = null;
   }
-  if (book) {
-    Book.instances[book.isbn] = book;
-    console.log( `${book.toString()} created!`);
+  if (movie) {
+    Movie.instances[movie.movieId] = movie;
+    console.log( `${movie.toString()} created!`);
   }
 };
 /**
- *  Update an existing book row
+ *  Update an existing movie row
  */
-Book.update = function (slots) {
+Movie.update = function (slots) {
   var noConstraintViolated = true,
       updatedProperties = [];
-  const book = Book.instances[slots.isbn],
-        objectBeforeUpdate = cloneObject( book);
+  const movie = Movie.instances[slots.movieId],
+        objectBeforeUpdate = cloneObject( movie);
   try {
-    if (book.title !== slots.title) {
-      book.setTitle( slots.title);
+    if (movie.title !== slots.title) {
+      movie.setTitle( slots.title);
       updatedProperties.push("title");
     }
-    if (book.year !== parseInt(slots.year)) {
-      book.setYear( slots.year);
-      updatedProperties.push("year");
+    if (movie.releaseDate !== parseInt(slots.releaseDate)) {
+      movie.setReleaseDate( slots.releaseDate);
+      updatedProperties.push("releaseDate");
     }
-    if (slots.edition && parseInt( slots.edition) !== book.edition) {
-      // slots.edition has a non-empty value that is new
-      book.setEdition( slots.edition);
-      updatedProperties.push("edition");
-    } else if (!slots.edition && book.edition) {
-      // slots.edition has an empty value that is new
-      delete book.edition;  // unset the property "edition"
-      updatedProperties.push("edition");
-	  }
+    // if (slots.edition && parseInt( slots.edition) !== movie.edition) {
+    //   // slots.edition has a non-empty value that is new
+    //   movie.setEdition( slots.edition);
+    //   updatedProperties.push("edition");
+    // } else if (!slots.edition && movie.edition) {
+    //   // slots.edition has an empty value that is new
+    //   delete movie.edition;  // unset the property "edition"
+    //   updatedProperties.push("edition");
+	  // }
   } catch (e) {
     console.log( `${e.constructor.name}: ${e.message}`);
     noConstraintViolated = false;
     // restore object to its state before updating
-    Book.instances[slots.isbn] = objectBeforeUpdate;
+     Movie.instances[movie.movieId] = objectBeforeUpdate;
   }
   if (noConstraintViolated) {
     if (updatedProperties.length > 0) {
-      console.log(`Properties ${updatedProperties.toString()} modified for book ${slots.isbn}`);
+      console.log(`Properties ${updatedProperties.toString()} modified for movie ${slots.movieId}`);
     } else {
-      console.log(`No property value changed for book ${slots.isbn}!`);
+      console.log(`No property value changed for movie ${slots.movieId}!`);
     }
   }
 };
 /**
- *  Delete a book
+ *  Delete a movie
  */
-Book.destroy = function (isbn) {
-  if (Book.instances[isbn]) {
-    console.log( `${Book.instances[isbn].toString()} deleted!`);
-    delete Book.instances[isbn];
+Movie.destroy = function (movieId) {
+  if (Movie.instances[movieId]) {
+    console.log( `${Movie.instances[movieId].toString()} deleted!`);
+    delete Movie.instances[movieId];
   } else {
-    console.log(`There is no book with ISBN ${isbn} in the database!`);
+    console.log(`There is no movie with ID ${movieId} in the database!`);
   }
 };
 /**
  *  Convert row to object
  */
-Book.convertRec2Obj = function (bookRow) {
-  var book={};
+Movie.convertRec2Obj = function (movieRow) {
+  var movie={};
   try {
-    book = new Book( bookRow);
+    movie = new Movie( movieRow);
   } catch (e) {
-    console.log(`${e.constructor.name} while deserializing a book row: ${e.message}`);
+    console.log(`${e.constructor.name} while deserializing a movie row: ${e.message}`);
   }
-  return book;
+  return movie;
 };
 /**
- *  Load all book table rows and convert them to objects
+ *  Load all movie table rows and convert them to objects
  */
-Book.retrieveAll = function () {
-  var booksString="";
+Movie.retrieveAll = function () {
+  var moviesString="";
   try {
-    if (localStorage["books"]) {
-      booksString = localStorage["books"];
+    if (localStorage["movies"]) {
+      moviesString = localStorage["movies"];
     }
   } catch (e) {
     alert("Error when reading from Local Storage\n" + e);
   }
-  if (booksString) {
-    const books = JSON.parse( booksString);
-    console.log( `${Object.keys( books).length} books loaded.`);
-    for (let key of Object.keys( books)) {
-      Book.instances[key] = Book.convertRec2Obj( books[key]);
+  if (moviesString) {
+    const movies = JSON.parse( moviesString);
+    console.log( `${Object.keys( movies).length} movies loaded.`);
+    for (let key of Object.keys( movies)) {
+      Movie.instances[key] = Movie.convertRec2Obj( movies[key]);
     }
   }
 };
 /**
- *  Save all book objects
+ *  Save all movie objects
  */
-Book.saveAll = function () {
-  var booksString="", error=false;
-  const nmrOfBooks = Object.keys( Book.instances).length;
+Movie.saveAll = function () {
+  var moviesString="", error=false;
+  const nmrOfMovies = Object.keys( Movie.instances).length;
   try {
-    booksString = JSON.stringify( Book.instances);
-    localStorage["books"] = booksString;
+    moviesString = JSON.stringify( Movie.instances);
+    localStorage["movies"] = moviesString;
   } catch (e) {
     alert("Error when writing to Local Storage\n" + e);
     error = true;
   }
-  if (!error) console.log( `${nmrOfBooks} books saved.`);
+  if (!error) console.log( `${nmrOfMovies} movies saved.`);
 };
 /*******************************************
 *** Auxiliary methods for testing **********
@@ -270,25 +270,29 @@ Book.saveAll = function () {
 /**
  *  Create and save test data
  */
-Book.generateTestData = function () {
+Movie.generateTestData = function () {
   try {
-    Book.instances["006251587X"] = new Book({
-      isbn: "006251587X",
-      title: "Weaving the Web",
-      year: 2000,
-      edition: 2
+    Movie.instances["1"] = new Movie({
+      movieId: "1",
+      title: "Pulp Fiction",
+      releaseDate: 1994-05-12,
     });
-    Book.instances["0465026567"] = new Book({
-      isbn: "0465026567",
-      title: "Gödel, Escher, Bach",
-      year: 1999
+    Movie.instances["2"] = new Movie({
+      movieId: "2",
+      title: "Star Wars",
+      releaseDate: 1977-05-25,
     });
-    Book.instances["0465030793"] = new Book({
-      isbn: "0465030793",
-      title: "I Am A Strange Loop",
-      year: 2008
+    Movie.instances["3"] = new Movie({
+      movieId: "3",
+      title: "Casablanca",
+      releaseDate: 1943-01-23,
     });
-    Book.saveAll();
+    Movie.instances["4"] = new Movie({
+      movieId: "4",
+      title: "The Godfather",
+      releaseDate: 1972-03-15,
+    });
+    Movie.saveAll();
   } catch (e) {
     console.log(`${e.constructor.name}: ${e.message}`);
   }
@@ -296,11 +300,11 @@ Book.generateTestData = function () {
 /**
  * Clear data
  */
-Book.clearData = function () {
-  if (confirm("Do you really want to delete all book data?")) {
+Movie.clearData = function () {
+  if (confirm("Do you really want to delete all movie data?")) {
     try {
-      Book.instances = {};
-      localStorage["books"] = "{}";
+      Movie.instances = {};
+      localStorage["movies"] = "{}";
       console.log("All data cleared.");
     } catch (e) {
       console.log(`${e.constructor.name}: ${e.message}`);
@@ -308,4 +312,4 @@ Book.clearData = function () {
   }
 };
 
-export default Book;
+export default Movie;
